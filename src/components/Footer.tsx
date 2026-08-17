f4a71b5317ca4a4f2a9ca68b7284cbcd54fe8684
lianlele168 <lianlele168@gmail.com>
2026-08-17 20:34:39 +0800

    style: remove network matrix bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Flame, ExternalLink, Heart } from 'lucide-react';
-import MatrixNav from './MatrixNav';
 
 export default function Footer() {
   return (
@@ -96,8 +95,6 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-
-      <MatrixNav />
 
       <div className="max-w-7xl mx-auto pt-4 flex flex-col sm:flex-row items-center justify-between text-xs text-slate-500 gap-4">
         <div>&copy; {new Date().getFullYear()} Type Soul Wiki. All rights reserved.</div>
